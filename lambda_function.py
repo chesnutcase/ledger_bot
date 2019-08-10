1f6e8a0ebd972b537cc8f9689dd676e600fc6a7a
chesnutcase <chester8991@gmail.com>
2019-08-10 17:57:22 +0800

    version 2

--- lambda_function.py
+++ lambda_function.py
@@ -86,13 +86,17 @@ def lambda_handler(event, context):
                     sender_name = "@{}".format(message["from"]["username"]) if "username" in message["from"] else "{} {}".format(message["from"].get("first_name", ""), message["from"].get("last_name", ""))
                     account = tableutils.view_account(gid, sender)
                     uids = account.keys()
-                    values = account.values()
+                    #values = account.values()
                     usernames = tableutils.resolve_ids_to_names(list(uids))
-                    lines = ["{:10}  {}".format(username, value*-1) for username, value in zip(usernames, values) if username is not None]
+                    uids_to_usernames = dict(zip(uids, usernames))
+                    debit_lines = ["{:10}  {}".format(uids_to_usernames[k], abs(v)) for k, v in account.items() if v < 0]
+                    debit_block = "\nYou owe these people money:\n{}".format("\n".join(debit_lines)) if len(debit_lines) > 0 else ""
+                    credit_lines = ["{:10}  {}".format(uids_to_usernames[k], v) for k, v in account.items() if v > 0]
+                    credit_block = "\nThese people owe you money:\n{}".format("\n".join(credit_lines)) if len(credit_lines) > 0 else ""
                     payload = {
                         'method': 'sendMessage',
                         'chat_id': gid,
-                        'text': "<b>Debts for {}</b> \n\n{}".format(sender_name, "\n".join(lines)),
+                        'text': "<b>Debts for {}</b> \n{}\n{}".format(sender_name, debit_block, credit_block),
                         'parse_mode': 'HTML'
                     }
                     return payload
