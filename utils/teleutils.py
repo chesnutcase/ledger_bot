1e1dfce8fa7106fabb66b4881c8d81c585239c4d
chesnutcase <chester8991@gmail.com>
2019-08-11 16:21:03 +0800

    add call_method method

--- utils/teleutils.py
+++ utils/teleutils.py
@@ -26,3 +26,14 @@ def resolve_ids_to_names(ids, gid):
             responseData = json.loads(the_page)["result"]
             names.append(responseData["user"].get("username") or "{} {}".format(responseData["user"].get("first_name"), responseData["user"].get("last_name")))
     return dict(zip(ids, names))
+
+
+def call_method(method, payload):
+    url = "{}/{}".format(API_URL, method)
+    data = urllib.parse.urlencode(payload)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        responseData = json.loads(the_page)["result"]
+        return responseData
